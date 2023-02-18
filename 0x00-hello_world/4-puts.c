ec9c23d048dbbfc1010736f3aeff335a95b8d4f0
sofiberry <111701716+sofiberry@users.noreply.github.com>
2023-02-18 01:59:37 -0800

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+
 /**
- * main - Entry
- * Return: Always 0
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
