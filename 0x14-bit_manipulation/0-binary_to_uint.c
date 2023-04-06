69f49848f2b216f1e479bcef7ea88d2a7b4056bc
sofiberry <sofi2darling@gmail.com>
2023-04-06 11:03:10 -0700

    init file

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,13 +1,37 @@
-#ifndef __MAIN_H__
-#define __MAIN_H__
+#include "main.h"
 
-unsigned int binary_to_uint(const char *b);
-void print_binary(unsigned long int n);
-int _putchar(char c);
-int get_bit(unsigned long int n, unsigned int index);
-int set_bit(unsigned long int *n, unsigned int index);
-int clear_bit(unsigned long int *n, unsigned int index);
-unsigned int flip_bits(unsigned long int n, unsigned long int m);
-int get_endianness(void);
+/**
+ *  * binary_to_uint - converts a binary number to an
+ *   * unsigned int.
+ *    * @b: binary.
+ *     *
+ *      * Return: unsigned int.
+ *       */
+unsigned int binary_to_uint(const char *b)
+{
+		unsigned int ui;
+			int len, base_two;
 
-#endif
+				if (!b)
+							return (0);
+
+					ui = 0;
+
+						for (len = 0; b[len] != '\0'; len++)
+									;
+
+							for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+									{
+												if (b[len] != '0' && b[len] != '1')
+															{
+																			return (0);
+																					}
+
+														if (b[len] & 1)
+																	{
+																					ui += base_two;
+																							}
+															}
+
+								return (ui);
+}
